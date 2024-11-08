bbce677924d6550ad0ca7ef65316a17b66800c38
Koon Kiat <bookoonkiat@gmail.com>
2024-11-09 01:34:49 +0800

    Initialize RTC and Add Serial Monitor Startup Delay
    - Added a delay to ensure SerialMonitorInterface starts up correctly.
    - Initialized the RTC module.
    - Separated handling of sensor readings and display updates for improved performance.

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -10,14 +10,17 @@
 #include "include/config.h"                      // Include configuration file
 #include "include/medication.h"                  // Include Medication module
 
+
 void setup()
 {
     SerialMonitorInterface.begin(9600);
-    delay(2000);
+    delay(1000);
 
     Wire.begin();
     display.begin();
     display.setBrightness(10);
+
+    rtc.begin();
 
     SerialMonitorInterface.print("Initializing BMA...\n");
     accel_sensor.begin(BMA250_range_2g, BMA250_update_time_64ms);
@@ -25,9 +28,24 @@ void setup()
 
 void loop()
 {
-    checkFallDetectionAndTemperature();
-    handleMedicationConfirmation();
-    updateDisplay(temp, activityStatus.c_str());
+    unsigned long currentMillis = millis();
+
+    // Handle sensor reading every SENSOR_READ_INTERVAL
+    if (currentMillis - previousLoopTime >= SENSOR_READ_INTERVAL)
+    {
+        previousLoopTime = currentMillis;
+
+        checkFallDetectionAndTemperature();
+        handleMedicationConfirmation();
+    }
+
+    // Handle display updates every DISPLAY_UPDATE_INTERVAL
+    static unsigned long lastDisplayUpdate = 0;
+    if (currentMillis - lastDisplayUpdate >= DISPLAY_UPDATE_INTERVAL)
+    {
+        lastDisplayUpdate = currentMillis;
+        updateFallDisplayStatus();
+    }
 
     if (SerialMonitorInterface.available())
     {
