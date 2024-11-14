87189a537a751af1bc6215747c42119d30b8d246
choonshing <2402869@sit.singaporetech.edu.sg>
2024-11-15 02:47:43 +0800

    removed unnecessary files

--- arduino/include/dataSender.h
+++
@@ -1,8 +0,0 @@
-#ifndef DATASENDER_H
-#define DATASENDER_H
-
-#include <Arduino.h>
-
-void sendDataToServer(const String &fallStatus, const String &activityStatus, float temperature);
-
-#endif
--- arduino/include/jsonHandler.h
+++
@@ -1,8 +0,0 @@
-#ifndef JSONHANDLER_H
-#define JSONHANDLER_H
-
-#include <ArduinoJson.h>
-
-String createJsonPayload(const String &fallStatus, const String &activityStatus, float temperature);
-
-#endif
