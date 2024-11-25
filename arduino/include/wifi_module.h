8339b02724bab1eb2f6b3cf5989c70a8f2f55bd0
Koon Kiat <bookoonkiat@gmail.com>
2024-11-25 14:14:18 +0800

    Remove commented function declarations in wifi_module.h for clarity

--- arduino/include/wifi_module.h
+++ arduino/include/wifi_module.h
@@ -3,7 +3,6 @@
 
 #include <WiFi101.h>
 
-// Declare functions to initialize and manage WiFi
 void initializeWiFi();
 void printWiFiStatus(int status);
 void sendSensorData(const char* server, int port, const String& data);
