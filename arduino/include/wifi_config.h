be578caa84730997d6a36c053dd5bd15b5d10d12
Koon Kiat <bookoonkiat@gmail.com>
2024-11-25 14:13:26 +0800

    Update wifi_config.h to provide placeholders for WiFi credentials and server address

--- arduino/include/wifi_config.h
+++ arduino/include/wifi_config.h
@@ -2,11 +2,11 @@
 #define WiFiConfig_h
 
 // WiFi network credentials
-const char WIFI_SSID[] = "Linksys9";
-const char WIFI_PASSWORD[] = "password";
+const char WIFI_SSID[] = ""; // Add your WiFi SSID
+const char WIFI_PASSWORD[] = ""; // Add your WiFi password
 
 // Define the server address and port
-const char SERVER_ADDRESS[] = "ultiswinger.pythonanywhere.com";
-const int SERVER_PORT = 80;
+const char SERVER_ADDRESS[] = ""; // Add your server address
+const int SERVER_PORT = 80; // Add your server port
 
 #endif
